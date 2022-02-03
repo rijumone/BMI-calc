b95d0aff2f101b7bd0af13f9ca79371321f47b99
Rijumone Choudhuri <rijumone@pop-os.localdomain>
2022-02-03 22:30:11 +0530

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+'''setup to install module'''
 import pathlib
 from setuptools import setup
 
@@ -8,8 +9,9 @@ README = (HERE / "README.md").read_text()
 setup(
     name='BMI Calc',
     version='0.1',
-    description='',
-    url='',
+    description='Python module to load JSON data into a Pandas dataframe. \
+        BMI is subsequently calculated for each record.',
+    url='https://github.com/rijumone/BMI-calc',
     author='Rijumone Choudhuri',
     author_email='mailmeonriju@gmail.com',
     license='MIT',
